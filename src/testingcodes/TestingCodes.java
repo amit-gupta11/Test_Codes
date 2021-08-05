a954018a7581c49d4b46bef587856cbef8def4c6
Lenovo <ag98888@gmail.com>
2021-08-05 17:31:43 +0530

    Testing Codes Of Topics.

--- src/testingcodes/TestingCodes.java
+++ src/testingcodes/TestingCodes.java
@@ -24,12 +24,12 @@ public static void main(String [] args){
   TelephoneBook.put("MARA", "670967548730");
   TelephoneBook.put("KARA", "8765748730");
   
-  Scanner sc = new Scanner(System.in);
+  Scanner scanrecord = new Scanner(System.in);
   System.out.print("Enter Name=");
-  String name=sc.next().toUpperCase();
+  String name=scanrecord.next().toUpperCase();
     
   if(TelephoneBook.containsKey(name)) {
-   System.out.println(name +"= "+TelephoneBook.get(name));
+   System.out.println(name +" = "+TelephoneBook.get(name));
   }
   else{
       System.out.println("Not FOUND");
